a40d91344f6f4df9167d8c4b27eec3e463a454e3
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-18 17:43:19 +0100

    ft: export buttons

--- projects/ngx-semantic/src/lib/button/button.component.ts
+++ projects/ngx-semantic/src/lib/button/button.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'sui-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  selector: '[sui-button]',
+  template: `
+    <ng-content></ng-content>
+  `
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
   }
