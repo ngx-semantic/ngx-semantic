e905e8b8a8e0fd1481f2ae572a791940aeb50dfa
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-18 21:29:07 +0100

    ft: add standard button support

--- projects/ngx-semantic/src/button/button.component.ts
+++ projects/ngx-semantic/src/button/button.component.ts
@@ -1,16 +1,19 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
 
+export type SuiButtonType = 'primary' | 'secondary' | 'animated' | 'labeled' | 'basic' | null;
+export type SuiButtonSize = 'mini' | 'tiny' | 'small' | 'medium' | 'large' | 'big' | 'huge' | 'massive' | null;
+
 @Component({
-  selector: '[sui-button]',
+  selector: 'button[sui-button], a[sui-button]',
   template: `
     <ng-content></ng-content>
-  `
-})
-export class SuiButtonComponent implements OnInit {
-
-  constructor(private el: ElementRef) { }
-
-  ngOnInit(): void {
+  `,
+  host: {
+    '[class.ui]': `true`,
+    '[class.button]': `true`
   }
-
+})
+export class SuiButtonComponent {
+  constructor(private el: ElementRef) {
+  }
 }
