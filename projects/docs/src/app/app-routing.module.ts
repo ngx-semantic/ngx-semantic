6605287d4f31db7dd60edfe9922b4bc983e825d6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-19 12:42:55 +0100

    ft: add buttons page routing

--- projects/docs/src/app/app-routing.module.ts
+++ projects/docs/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-
+import {ElementsComponent} from './elements/elements.component';
+import {ButtonsComponent} from './elements/buttons/buttons.component';
 
 const routes: Routes = [
   {
-
+    path: 'elements',
+    component: ElementsComponent,
+    children: [
+      {
+        path: 'buttons',
+        component: ButtonsComponent
+      }
+    ]
   }
 ];
 
