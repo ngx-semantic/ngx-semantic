8d162e0235bb72038110e908ebadd1c1e145f151
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-20 10:29:28 +0100

    ft: add the button group details

--- projects/ngx-semantic/button/button-group.component.ts
+++ projects/ngx-semantic/button/button-group.component.ts
@@ -2,13 +2,19 @@
  * Created by bolor on 4/20/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: '[sui-button-group]',
   template: `
     <ng-content></ng-content>
-  `
+  `,
+  host: {
+    '[class.ui]': `true`,
+    '[class.buttons]': `true`,
+    '[class.icon]': `suiIcon`
+  }
 })
 export class SuiButtonGroupComponent {
+  @Input() suiIcon = false;
 }
