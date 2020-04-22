337ff8be58a925f4ed7c243d9a45ff02eee7883c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-22 20:15:54 +0100

    ft: add file scaffold

---
+++ projects/ngx-semantic/src/divider/divider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sui-divider',
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.css']
+})
+export class DividerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ngx-semantic/src/divider/index.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by bolor on 4/22/2020
+ */
