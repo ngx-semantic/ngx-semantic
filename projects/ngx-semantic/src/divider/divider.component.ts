5da3dbe3581c6271572ceda76dc41173ac2831c8
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-22 20:40:31 +0100

    ft: finalize divider

--- projects/ngx-semantic/src/divider/divider.component.ts
+++ projects/ngx-semantic/src/divider/divider.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {SuiDirection} from '../common';
 
 @Component({
   selector: '[sui-divider]',
@@ -7,10 +8,28 @@ import {Component, OnInit} from '@angular/core';
   `,
   host: {
     '[class.ui]': `true`,
-    '[class.divider': `true`
+    '[class.divider]': `true`,
+    '[class.header]': `suiHeader`,
+    '[class.inverted]': `suiInverted`,
+    '[class.fitted]': `suiFitted`,
+    '[class.hidden]': `suiHidden`,
+    '[class.section]': `suiSection`,
+    '[class.clearing]': `suiClearing`,
+
+    // direction
+    '[class.vertical]': `suiDirection === 'vertical'`,
+    '[class.horizontal]': `suiDirection === 'horizontal'`
   }
 })
 export class SuiDividerComponent {
+  @Input() suiDirection: SuiDirection = null;
+  @Input() suiHeader = false;
+  @Input() suiInverted = false;
+  @Input() suiFitted = false;
+  @Input() suiHidden = false;
+  @Input() suiSection = false;
+  @Input() suiClearing = false;
+
   constructor() {
   }
 }
