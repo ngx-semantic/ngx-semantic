bcafe046eca873a067e0c0b686ce93ab5fc9de09
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-23 11:48:33 +0100

    ft: adding in tests

--- projects/ngx-semantic/src/flag/flag.spec.ts
+++ projects/ngx-semantic/src/flag/flag.spec.ts
@@ -1,25 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { FlagComponent } from './flag.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Component} from '@angular/core';
+import {SuiFlagComponent} from './flag.component';
+import {By} from '@angular/platform-browser';
 
 describe('FlagComponent', () => {
-  let component: FlagComponent;
-  let fixture: ComponentFixture<FlagComponent>;
+  let component: TestFlagComponent;
+  let fixture: ComponentFixture<TestFlagComponent>;
+  let flagElement: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FlagComponent ]
-    })
-    .compileComponents();
+        declarations: [TestFlagComponent, SuiFlagComponent]
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FlagComponent);
+    fixture = TestBed.createComponent(TestFlagComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    flagElement = fixture.debugElement.query(By.directive(SuiFlagComponent)).nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should apply class name', () => {
+    expect(flagElement.className).toBe('myanmar flag');
+  });
 });
+
+@Component({
+  template: `
+    <i class="myanmar"
+       sui-flag>
+    </i>
+  `
+})
+export class TestFlagComponent {
+}
--- projects/ngx-semantic/src/ngx-semantic.module.ts
+++ projects/ngx-semantic/src/ngx-semantic.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {SuiButtonModule} from './button';
 import {SuiDividerModule} from './divider';
+import {SuiFlagModule} from './flag';
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [
     SuiButtonModule,
-    SuiDividerModule
+    SuiDividerModule,
+    SuiFlagModule
   ]
 })
 export class NgxSemanticModule {
