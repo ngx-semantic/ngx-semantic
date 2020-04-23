cb973761c0cc0e0ef1ae4593be358f95cf550c96
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-23 13:49:29 +0100

    ft: I have to do this as it doesnt do a prod build without it

--- projects/ngx-semantic/src/public-api.ts
+++ projects/ngx-semantic/src/public-api.ts
@@ -2,8 +2,8 @@
  * Created by bolor on 4/22/2020
  */
 
-export * from './button';
-export * from './common';
-export * from './divider';
-export * from './flag';
-export * from './ngx-semantic.module';
+export * from './lib/button';
+export * from './lib/common';
+export * from './lib/divider';
+export * from './lib/flag';
+export * from './lib/ngx-semantic.module';
