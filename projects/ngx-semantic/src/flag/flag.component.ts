d2f3f73b0780ea82cf256d3a6e30461033f6c49a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-23 11:15:13 +0100

    ft: add flag component scaffold

---
+++ projects/ngx-semantic/src/flag/flag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sui-flag',
+  templateUrl: './flag.component.html',
+  styleUrls: ['./flag.component.css']
+})
+export class FlagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
