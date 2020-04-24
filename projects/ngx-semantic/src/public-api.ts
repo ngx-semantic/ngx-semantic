d7bc26ed53803df28f18faf668e5e6962d7651e7
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-24 16:34:11 +0100

    ft: fix header import

--- projects/ngx-semantic/src/public-api.ts
+++ projects/ngx-semantic/src/public-api.ts
@@ -6,5 +6,5 @@ export * from './lib/button';
 export * from './lib/common';
 export * from './lib/divider';
 export * from './lib/flag';
-export * from './header';
+export * from './lib/header';
 export * from './lib/ngx-semantic.module';
