0eb2b2bdc59526680685e2154ed271df01fb6306
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-24 13:05:38 +0100

    ft: finalize header

--- projects/ngx-semantic/src/header/header.component.ts
+++ projects/ngx-semantic/src/header/header.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
-import {SuiDirection} from '../common';
+import {SuiColour, SuiLocation, SuiSize} from '../common';
+
+export type SuiHeaderAlignment = 'left' | 'right' | 'center' | 'justified' | null;
 
 @Component({
   selector: '[sui-header]',
@@ -8,27 +10,65 @@ import {SuiDirection} from '../common';
   `,
   host: {
     '[class.ui]': `true`,
-    '[class.divider]': `true`,
-    '[class.header]': `suiHeader`,
+    '[class.header]': `true`,
+    '[class.icon]': `suiIcon`,
+    '[class.sub]': `suiSubHeader`,
+    '[class.dividing]': `suiDividing`,
+    '[class.block]': `suiBlock`,
+    '[class.attached]': `suiAttached`,
+    '[class.floated]': `suiFloated`,
     '[class.inverted]': `suiInverted`,
-    '[class.fitted]': `suiFitted`,
-    '[class.hidden]': `suiHidden`,
-    '[class.section]': `suiSection`,
-    '[class.clearing]': `suiClearing`,
 
-    // direction
-    '[class.vertical]': `suiDirection === 'vertical'`,
-    '[class.horizontal]': `suiDirection === 'horizontal'`
+    // states
+    '[class.disabled]': `suiDisabled`,
+
+    // sizes
+    '[class.mini]': `suiSize == 'mini'`,
+    '[class.tiny]': `suiSize == 'tiny'`,
+    '[class.small]': `suiSize == 'small'`,
+    '[class.medium]': `suiSize == 'medium'`,
+    '[class.large]': `suiSize == 'large'`,
+    '[class.big]': `suiSize == 'big'`,
+    '[class.huge]': `suiSize == 'huge'`,
+    '[class.massive]': `suiSize == 'massive'`,
+
+    // locations
+    '[class.top]': `suiLocation == 'top'`,
+    '[class.bottom]': `suiLocation == 'bottom'`,
+
+    // alignments
+    '[class.aligned]': `!!suiAlignment && suiAlignment !== 'justified'`,
+    '[class.left]': `suiAlignment === 'left'`,
+    '[class.right]': `suiAlignment === 'right'`,
+    '[class.center]': `suiAlignment === 'center'`,
+
+    // colours
+    '[class.red]': `suiColour == 'red'`,
+    '[class.orange]': `suiColour == 'orange'`,
+    '[class.yellow]': `suiColour == 'yellow'`,
+    '[class.olive]': `suiColour == 'olive'`,
+    '[class.green]': `suiColour == 'green'`,
+    '[class.teal]': `suiColour == 'teal'`,
+    '[class.blue]': `suiColour == 'blue'`,
+    '[class.pink]': `suiColour == 'pink'`,
+    '[class.brown]': `suiColour == 'brown'`,
+    '[class.grey]': `suiColour == 'grey'`,
+    '[class.black]': `suiColour == 'black'`
   }
 })
 export class SuiHeaderComponent {
-  @Input() suiDirection: SuiDirection = null;
-  @Input() suiHeader = false;
+  @Input() suiSize: SuiSize = null;
+  @Input() suiLocation: SuiLocation = null;
+  @Input() suiAlignment: SuiHeaderAlignment = null;
+  @Input() suiColour: SuiColour = null;
+  @Input() suiIcon = false;
+  @Input() suiSubHeader = false;
+  @Input() suiDisabled = false;
+  @Input() suiDividing = false;
+  @Input() suiBlock = false;
+  @Input() suiAttached = false;
+  @Input() suiFloated = false;
   @Input() suiInverted = false;
-  @Input() suiFitted = false;
-  @Input() suiHidden = false;
-  @Input() suiSection = false;
-  @Input() suiClearing = false;
 
   constructor() {
   }
