345b707f63b5e45d4aee939f395ce0488f6120ba
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-25 18:50:18 +0100

    ft: add files

---
+++ projects/ngx-semantic/src/lib/icon/icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sui-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.css']
+})
+export class IconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
