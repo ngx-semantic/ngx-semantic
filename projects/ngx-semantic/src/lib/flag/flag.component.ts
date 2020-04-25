d2fc0877d6417d51d0fea7a8396109b5c2ac30c6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-25 20:51:07 +0100

    ft: fix the flag logic

--- projects/ngx-semantic/src/lib/flag/flag.component.ts
+++ projects/ngx-semantic/src/lib/flag/flag.component.ts
@@ -1,15 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: '[sui-flag]',
   template: `
     <ng-content></ng-content>
-  `,
-  host: {
-    '[class.flag]': `true`
-  }
+  `
 })
-export class SuiFlagComponent {
+export class SuiFlagComponent implements OnInit {
+  @HostBinding('class') classes = 'flag';
+  @Input() suiCountry = '';
+
   constructor() {
+  }
+
+  ngOnInit(): void {
+    if (this.suiCountry) {
+      this.classes += this.suiCountry;
+    }
   }
 }
