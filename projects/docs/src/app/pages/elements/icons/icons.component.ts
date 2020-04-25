8a15878b288ac4146bf2792efabb9b85ba914122
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-25 21:00:27 +0100

    ft: add icon page scaffold

---
+++ projects/docs/src/app/pages/elements/icons/icons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'doc-icons',
+  templateUrl: './icons.component.html',
+  styleUrls: ['./icons.component.scss']
+})
+export class IconsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
