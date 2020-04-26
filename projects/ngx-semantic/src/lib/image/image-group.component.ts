334d71d000e353600823d20cb368847ac79a2f2f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-26 14:56:28 +0100

    ft: add component and module scaffold

--- projects/ngx-semantic/src/lib/image/image-group.component.ts
+++ projects/ngx-semantic/src/lib/image/image-group.component.ts
@@ -1,3 +1,14 @@
 /**
  * Created by bolor on 4/26/2020
  */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[sui-image-group]',
+  template: `
+    <ng-content></ng-content>
+  `
+})
+export class SuiImageGroupComponent {
+}
--- projects/ngx-semantic/src/lib/image/index.ts
+++ projects/ngx-semantic/src/lib/image/index.ts
@@ -2,3 +2,4 @@
  * Created by bolor on 4/26/2020
  */
 
+export * from './public-api';
