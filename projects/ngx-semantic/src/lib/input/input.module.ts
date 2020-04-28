b84511e950117996b5767fe280a8793693c4f31e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-28 09:41:15 +0100

    ft: add module

--- projects/ngx-semantic/src/lib/input/input.module.ts
+++ projects/ngx-semantic/src/lib/input/input.module.ts
@@ -1,3 +1,15 @@
 /**
  * Created by bolor on 4/28/2020
  */
+
+import {NgModule} from '@angular/core';
+import {SuiInputComponent} from './input.component';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [SuiInputComponent],
+  imports: [CommonModule],
+  exports: [SuiInputComponent]
+})
+export class SuiInputModule {
+}
