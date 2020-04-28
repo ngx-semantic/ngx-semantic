41e81190800ccf5a69e9343c2014ce2470942967
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-28 09:40:19 +0100

    ft: add input scaffold

--- projects/ngx-semantic/src/lib/input/input.component.ts
+++ projects/ngx-semantic/src/lib/input/input.component.ts
@@ -1,3 +1,14 @@
 /**
  * Created by bolor on 4/28/2020
  */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[sui-input]',
+  template: `
+    <ng-content></ng-content>
+  `
+})
+export class SuiInputComponent {
+}
