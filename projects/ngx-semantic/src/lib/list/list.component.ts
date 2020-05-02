594d3ac560be9fd0b16b057e48aa07ef293ce89f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-02 08:57:06 +0100

    ft: finalize the list module

--- projects/ngx-semantic/src/lib/list/list.component.ts
+++ projects/ngx-semantic/src/lib/list/list.component.ts
@@ -1,3 +1,15 @@
 /**
  * Created by bolor on 5/2/2020
  */
+
+import {Component} from '@angular/core';
+
+Component({
+  selector: '[sui-list]',
+  template: `
+  <ng-content></ng-content>
+  `
+})
+export class SuiListComponent {
+
+}
