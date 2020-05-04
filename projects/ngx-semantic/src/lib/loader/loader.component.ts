83f611bb78b4574dd4c9d9545d54c905d1cdba0e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-04 13:37:11 +0100

    ft: add support for active text and indeterminate

--- projects/ngx-semantic/src/lib/loader/loader.component.ts
+++ projects/ngx-semantic/src/lib/loader/loader.component.ts
@@ -2,13 +2,35 @@
  * Created by bolor on 5/4/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 
 @Component({
-  selector: '[sui-loader]',
+  selector: 'div[sui-loader]',
   template: `
-
+    <ng-content></ng-content>
   `
 })
 export class SuiLoaderComponent {
+  @Input() suiText = false;
+  @Input() suiIndeterminate = false;
+  @Input() suiActive = false;
+
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'ui',
+      this.getPropClass(this.suiIndeterminate, 'indeterminate'),
+      this.getPropClass(this.suiText, 'text'),
+      this.getPropClass(this.suiActive, 'active'),
+      'loader'
+    ].join(' ');
+  }
+
+  getPropClass(state: boolean, className: string): string {
+    if (!state) {
+      return '';
+    }
+
+    return className;
+  }
 }
