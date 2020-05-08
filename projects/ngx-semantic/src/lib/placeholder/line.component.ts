ef67f20d359205ea2dbbf1be387a9759611788a1
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-08 10:47:36 +0100

    ft: add better line field support

--- projects/ngx-semantic/src/lib/placeholder/line.component.ts
+++ projects/ngx-semantic/src/lib/placeholder/line.component.ts
@@ -2,16 +2,24 @@
  * Created by bolor on 5/8/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+
+export type SuiPlaceholderLineLength = 'full' | 'very long' | 'long' | 'medium' | 'short' | 'very short' | null;
 
 @Component({
   selector: '[sui-placeholder-line]',
   template: `
     <ng-content></ng-content>
-  `,
-  host: {
-    '[class.line]': `true`
-  }
+  `
 })
 export class SuiPlaceholderLineComponent {
+  @Input() suiLength: SuiPlaceholderLineLength = null;
+
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      this.suiLength,
+      'line'
+    ].join(' ');
+  }
 }
