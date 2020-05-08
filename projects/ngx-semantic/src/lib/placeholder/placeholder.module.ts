35015c4a8d947cfcd706a2a9ec11913170fbcb50
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-08 10:41:59 +0100

    ft: add the paragraph support

--- projects/ngx-semantic/src/lib/placeholder/placeholder.module.ts
+++ projects/ngx-semantic/src/lib/placeholder/placeholder.module.ts
@@ -5,11 +5,12 @@
 import {NgModule} from '@angular/core';
 import {SuiPlaceholderComponent} from './placeholder.component';
 import {CommonModule} from '@angular/common';
+import {SuiPlaceholderParagraphComponent} from './paragraph.component';
 
 @NgModule({
-  declarations: [SuiPlaceholderComponent],
+  declarations: [SuiPlaceholderComponent, SuiPlaceholderParagraphComponent],
   imports: [CommonModule],
-  exports: [SuiPlaceholderComponent]
+  exports: [SuiPlaceholderComponent, SuiPlaceholderParagraphComponent]
 })
 export class SuiPlaceholderModule {
 }
--- projects/ngx-semantic/src/lib/placeholder/public-api.ts
+++ projects/ngx-semantic/src/lib/placeholder/public-api.ts
@@ -2,5 +2,6 @@
  * Created by bolor on 5/6/2020
  */
 
+export * from './paragraph.component';
 export * from './placeholder.component';
 export * from './placeholder.module';
