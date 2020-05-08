395d7fa0b0769a4d6282af29ee84e4b1b4b0bce7
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-08 11:52:00 +0100

    ft: finalize placeholder images

---
+++ projects/ngx-semantic/src/lib/placeholder/image.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by bolor on 5/8/2020
+ */
+
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: '[sui-placeholder-line]',
+  template: `
+    <ng-content></ng-content>
+  `,
+  host: {
+    '[class.square]': `suiSquare`,
+    '[class.rectangular]': `suiRectangular`,
+    '[class.image]': `true`,
+    '[class.header]': `suiHeader`,
+  }
+})
+export class SuiPlaceholderImageComponent {
+  @Input() suiSquare = false;
+  @Input() suiRectangular = false;
+  @Input() suiHeader = false;
+}
