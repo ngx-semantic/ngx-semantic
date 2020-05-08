5e67a8d1fd99ec1cb093e5f74b92f340d69a16d7
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-08 08:56:39 +0100

    ft: add rail scaffold

--- projects/ngx-semantic/src/lib/rail/rail.component.ts
+++ projects/ngx-semantic/src/lib/rail/rail.component.ts
@@ -1,3 +1,14 @@
 /**
  * Created by bolor on 5/8/2020
  */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[sui-rail]',
+  template: `
+    <ng-content></ng-content>
+  `
+})
+export class SuiRailComponent {
+}
