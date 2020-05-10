8fdc42e7bcb77aea9727a8446bbe42a32942abc7
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-10 23:01:16 +0100

    ft: add the rail details

--- projects/ngx-semantic/src/lib/rail/rail.component.ts
+++ projects/ngx-semantic/src/lib/rail/rail.component.ts
@@ -2,13 +2,44 @@
  * Created by bolor on 5/8/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+import {SuiHorizontalPosition, SuiSize} from '../common';
+
+export type SuiRailCloseness = 'close' | 'very close' | null;
 
 @Component({
-  selector: '[sui-rail]',
+  selector: 'div[sui-rail]',
   template: `
     <ng-content></ng-content>
   `
 })
 export class SuiRailComponent {
+  @Input() suiLocation: SuiHorizontalPosition = null;
+  @Input() suiSize: SuiSize = null;
+  @Input() suiCloseness: SuiRailCloseness = null;
+  @Input() suiInternal = false;
+  @Input() suiDividing = false;
+  @Input() suiAttached = false;
+
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'ui',
+      this.suiLocation,
+      this.getPropClass(this.suiInternal, 'internal'),
+      this.getPropClass(this.suiDividing, 'dividing'),
+      this.getPropClass(this.suiAttached, 'attached'),
+      this.suiCloseness,
+      this.suiSize,
+      'rail'
+    ].join(' ');
+  }
+
+  getPropClass(state: boolean, className: string): string {
+    if (!state) {
+      return '';
+    }
+
+    return className;
+  }
 }
