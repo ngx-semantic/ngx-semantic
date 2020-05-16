f665fe414f68ec7476d627505fce4dc1e845cd3e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-17 00:39:41 +0100

    ft: update public api

--- projects/ngx-semantic/src/lib/reveal/index.ts
+++ projects/ngx-semantic/src/lib/reveal/index.ts
@@ -1,3 +1,5 @@
 /**
  * Created by bolor on 5/17/2020
  */
+
+export * from './public-api';
--- projects/ngx-semantic/src/lib/reveal/public-api.ts
+++ projects/ngx-semantic/src/lib/reveal/public-api.ts
@@ -1,3 +1,6 @@
 /**
  * Created by bolor on 5/17/2020
  */
+
+export * from './reveal.component';
+export * from './reveal.module';
