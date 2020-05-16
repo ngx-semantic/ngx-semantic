79cb02fd022c8a37e8bf349378551906bd3a1a7a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-17 00:42:27 +0100

    ft: nadd in the scaffold

--- projects/ngx-semantic/src/lib/reveal/reveal.component.ts
+++ projects/ngx-semantic/src/lib/reveal/reveal.component.ts
@@ -1,3 +1,15 @@
 /**
  * Created by bolor on 5/17/2020
  */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[sui-reveal]',
+  template: `
+  <ng-content></ng-content>
+  `
+})
+export class SuiRevealComponent {
+
+}
--- projects/ngx-semantic/src/lib/reveal/reveal.module.ts
+++ projects/ngx-semantic/src/lib/reveal/reveal.module.ts
@@ -1,3 +1,15 @@
 /**
  * Created by bolor on 5/17/2020
  */
+
+import {NgModule} from '@angular/core';
+import {SuiRevealComponent} from './reveal.component';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [SuiRevealComponent],
+  imports: [CommonModule],
+  exports: [SuiRevealComponent]
+})
+export class SuiRevealModule {
+}
