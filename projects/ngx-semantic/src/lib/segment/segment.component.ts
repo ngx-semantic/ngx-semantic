0fa55be655f64dbac6631a3b9b2abc972be22eee
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-18 14:31:58 +0100

    ft: add segment details

--- projects/ngx-semantic/src/lib/segment/segment.component.ts
+++ projects/ngx-semantic/src/lib/segment/segment.component.ts
@@ -2,7 +2,8 @@
  * Created by bolor on 5/18/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+import {SuiColour, Utils} from '../common';
 
 @Component({
   selector: '[sui-segment]',
@@ -11,5 +12,25 @@ import {Component} from '@angular/core';
   `
 })
 export class SuiSegmentComponent {
+  @Input() suiColour: SuiColour = null;
+  @Input() suiPlaceholder = false;
+  @Input() suiRaised = false;
+  @Input() suiStacked = false;
+  @Input() suiPiled = false;
+  @Input() suiVertical = false;
+  @Input() suiSecondary = false;
 
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'ui',
+      Utils.getPropClass(this.suiPlaceholder, 'placeholder'),
+      Utils.getPropClass(this.suiRaised, 'raised'),
+      Utils.getPropClass(this.suiStacked, 'stacked'),
+      Utils.getPropClass(this.suiPiled, 'piled'),
+      Utils.getPropClass(this.suiSecondary, 'secondary'),
+      Utils.getPropClass(this.suiVertical, 'vertical'),
+      'segment'
+    ].join(' ');
+  }
 }
