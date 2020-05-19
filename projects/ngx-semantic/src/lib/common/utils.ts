34abe6486d46161898f48c3cda2d6f652d8de3c0
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-19 12:23:09 +0100

    ft: add in extension method

--- projects/ngx-semantic/src/lib/common/utils.ts
+++ projects/ngx-semantic/src/lib/common/utils.ts
@@ -2,6 +2,18 @@
  * Created by bolor on 5/17/2020
  */
 
+declare global {
+  interface Array<T> {
+    joinWithWhitespaceCleanup(): string;
+  }
+}
+
+if (!Array.prototype.joinWithWhitespaceCleanup) {
+  Array.prototype.joinWithWhitespaceCleanup = function <T>(this: Array<T>): string {
+    return this.join(' ').replace(/  +/g, ' ');
+  };
+}
+
 export class Utils {
   static getPropClass(state: boolean, className: string): string {
     if (!state) {
