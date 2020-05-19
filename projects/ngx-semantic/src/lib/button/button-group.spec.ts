94f0f2059d362fa7ab0c3246f599f1af4517e76d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-19 11:28:15 +0100

    ft: update the button group tests

--- projects/ngx-semantic/src/lib/button/button-group.spec.ts
+++ projects/ngx-semantic/src/lib/button/button-group.spec.ts
@@ -34,34 +34,98 @@ describe('SuiButtonGroupComponent', () => {
   });
 
   it('should apply class name', () => {
-    expect(groupElement.className).toBe('ui buttons');
+    expect(groupElement.className).toContain('ui');
+    expect(groupElement.className).toContain('buttons');
   });
 
-  it('should apply class name by state', () => {
-    component.suiIcon = true;
+  it('should apply class name by icon type', () => {
+    component.suiIcon = 'icon';
     fixture.detectChanges();
     expect(groupElement.classList).toContain('icon');
+    component.suiIcon = 'labeled icon';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('icon');
+    expect(groupElement.classList).toContain('labeled');
   });
 
   it('should apply class name by direction', () => {
-    component.suiDirection = 'vertical';
+    component.suiVertical = true;
     fixture.detectChanges();
     expect(groupElement.classList).toContain('vertical');
   });
 
-  it('should apply class name if attached', () => {
-    component.suiAttached = true;
+  it('should apply class name by attachment', () => {
+    component.suiAttached = 'top attached';
     fixture.detectChanges();
+    expect(groupElement.classList).toContain('top');
+    expect(groupElement.classList).toContain('attached');
+    component.suiAttached = 'bottom attached';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('bottom');
     expect(groupElement.classList).toContain('attached');
   });
 
-  it('should apply class name by location', () => {
-    component.suiLocation = 'top';
+  it('should apply class name by colour', () => {
+    component.suiColour = 'red';
     fixture.detectChanges();
-    expect(groupElement.classList).toContain('top');
-    component.suiLocation = 'bottom';
+    expect(groupElement.classList).toContain('red');
+    component.suiColour = 'orange';
     fixture.detectChanges();
-    expect(groupElement.classList).toContain('bottom');
+    expect(groupElement.classList).toContain('orange');
+    component.suiColour = 'yellow';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('yellow');
+    component.suiColour = 'olive';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('olive');
+    component.suiColour = 'green';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('green');
+    component.suiColour = 'teal';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('teal');
+    component.suiColour = 'blue';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('blue');
+    component.suiColour = 'pink';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('pink');
+    component.suiColour = 'brown';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('brown');
+    component.suiColour = 'grey';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('grey');
+    component.suiColour = 'black';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('black');
+  });
+
+  it('should apply class name by size', () => {
+    component.suiSize = 'mini';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('mini');
+    component.suiSize = 'tiny';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('tiny');
+    component.suiSize = 'small';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('small');
+    component.suiSize = 'medium';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('medium');
+    component.suiSize = 'large';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('large');
+    component.suiSize = 'big';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('big');
+    component.suiSize = 'huge';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('huge');
+    component.suiSize = 'massive';
+    fixture.detectChanges();
+    expect(groupElement.classList).toContain('massive');
   });
 });
 
@@ -69,9 +133,11 @@ describe('SuiButtonGroupComponent', () => {
   template: `
     <div
       sui-button-group
-      [suiLocation]="suiLocation"
-      [suiDirection]="suiDirection"
       [suiIcon]="suiIcon"
+      [suiColour]="suiColour"
+      [suiSize]="suiSize"
+      [suiBasic]="suiBasic"
+      [suiVertical]="suiVertical"
       [suiAttached]="suiAttached">
       <button sui-button></button>
       <button sui-button></button>
@@ -79,8 +145,10 @@ describe('SuiButtonGroupComponent', () => {
   `
 })
 export class TestButtonGroupComponent {
-  @Input() suiDirection: any = null;
-  @Input() suiLocation: any = null;
-  @Input() suiIcon = false;
-  @Input() suiAttached = false;
+  @Input() suiSize: any = null;
+  @Input() suiColour: any = null;
+  @Input() suiAttached: any = null;
+  @Input() suiIcon: any = null;
+  @Input() suiBasic = false;
+  @Input() suiVertical = false;
 }
