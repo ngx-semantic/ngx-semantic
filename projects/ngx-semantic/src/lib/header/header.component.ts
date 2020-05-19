2c498d5a70b3d063465d4234f816ec32039a1a56
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-19 11:02:01 +0100

    ft: update the header component

--- projects/ngx-semantic/src/lib/header/header.component.ts
+++ projects/ngx-semantic/src/lib/header/header.component.ts
@@ -1,78 +1,45 @@
-import {Component, Input} from '@angular/core';
-import {SuiColour, SuiLocation, SuiSize} from '../common';
+import {Component, HostBinding, Input} from '@angular/core';
+import {SuiColour, SuiSize, Utils} from '../common';
 
-export type SuiHeaderAlignment = 'left' | 'right' | 'center' | 'justified' | null;
+export type SuiHeaderAlignment = 'left aligned' | 'right aligned' | 'center aligned' | 'justified' | null;
+export type SuiHeaderAttachment = 'attached' | 'top attached' | 'bottom attached' | null;
+export type SuiHeaderFloating = 'left floated' | 'right floated' | null;
 
 @Component({
   selector: '[sui-header]',
   template: `
     <ng-content></ng-content>
-  `,
-  host: {
-    '[class.ui]': `true`,
-    '[class.icon]': `suiIcon`,
-    '[class.sub]': `suiSubHeader`,
-    '[class.dividing]': `suiDividing`,
-    '[class.block]': `suiBlock`,
-    '[class.attached]': `suiAttached`,
-    '[class.floated]': `suiFloated`,
-    '[class.inverted]': `suiInverted`,
-
-    // states
-    '[class.disabled]': `suiDisabled`,
-
-    // sizes
-    '[class.mini]': `suiSize == 'mini'`,
-    '[class.tiny]': `suiSize == 'tiny'`,
-    '[class.small]': `suiSize == 'small'`,
-    '[class.medium]': `suiSize == 'medium'`,
-    '[class.large]': `suiSize == 'large'`,
-    '[class.big]': `suiSize == 'big'`,
-    '[class.huge]': `suiSize == 'huge'`,
-    '[class.massive]': `suiSize == 'massive'`,
-
-    // locations
-    '[class.top]': `suiLocation == 'top'`,
-    '[class.bottom]': `suiLocation == 'bottom'`,
-
-    // alignments
-    '[class.aligned]': `!!suiAlignment && suiAlignment !== 'justified'`,
-    '[class.left]': `suiAlignment === 'left'`,
-    '[class.right]': `suiAlignment === 'right'`,
-    '[class.center]': `suiAlignment === 'center'`,
-    '[class.justified]': `suiAlignment === 'justified'`,
-
-    // colours
-    '[class.red]': `suiColour == 'red'`,
-    '[class.orange]': `suiColour == 'orange'`,
-    '[class.yellow]': `suiColour == 'yellow'`,
-    '[class.olive]': `suiColour == 'olive'`,
-    '[class.green]': `suiColour == 'green'`,
-    '[class.teal]': `suiColour == 'teal'`,
-    '[class.blue]': `suiColour == 'blue'`,
-    '[class.pink]': `suiColour == 'pink'`,
-    '[class.brown]': `suiColour == 'brown'`,
-    '[class.grey]': `suiColour == 'grey'`,
-    '[class.black]': `suiColour == 'black'`,
-
-    // close this out
-    '[class.header]': `true`
-  }
+  `
 })
 export class SuiHeaderComponent {
   @Input() suiSize: SuiSize = null;
-  @Input() suiLocation: SuiLocation = null;
   @Input() suiAlignment: SuiHeaderAlignment = null;
   @Input() suiColour: SuiColour = null;
-  @Input() suiIcon = false;
+  @Input() suiAttached: SuiHeaderAttachment = null;
+  @Input() suiFloated: SuiHeaderFloating = null;
   @Input() suiSubHeader = false;
   @Input() suiDisabled = false;
   @Input() suiDividing = false;
   @Input() suiBlock = false;
-  @Input() suiAttached = false;
-  @Input() suiFloated = false;
   @Input() suiInverted = false;
+  @Input() suiIcon = false;
 
-  constructor() {
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'ui',
+      this.suiColour,
+      this.suiSize,
+      this.suiAttached,
+      this.suiFloated,
+      this.suiAlignment,
+      Utils.getPropClass(this.suiDividing, 'dividing'),
+      Utils.getPropClass(this.suiDisabled, 'disabled'),
+      Utils.getPropClass(this.suiInverted, 'inverted'),
+      Utils.getPropClass(this.suiBlock, 'block'),
+      Utils.getPropClass(this.suiSubHeader, 'sub'),
+      Utils.getPropClass(this.suiIcon, 'icon'),
+      'header'
+    ].join(' ');
   }
 }
