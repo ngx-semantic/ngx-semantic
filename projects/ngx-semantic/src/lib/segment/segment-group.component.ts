506104d570da7efb38f2b91b94a146425eba6716
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-23 16:08:40 +0100

    ft: export the segment group component

--- projects/ngx-semantic/src/lib/segment/segment-group.component.ts
+++ projects/ngx-semantic/src/lib/segment/segment-group.component.ts
@@ -1,3 +1,15 @@
 /**
  * Created by bolor on 5/23/2020
  */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[sui-segment-group]',
+  template: `
+  <ng-content></ng-content>
+  `
+})
+export class SuiSegmentGroupComponent {
+
+}
