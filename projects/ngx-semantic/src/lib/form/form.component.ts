d2bea7384d85f878c15cb3cc44fc867be28d359b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-06-05 21:37:11 +0100

    ft: update component

--- projects/ngx-semantic/src/lib/form/form.component.ts
+++ projects/ngx-semantic/src/lib/form/form.component.ts
@@ -2,7 +2,10 @@
  * Created by bolor on 6/5/2020
  */
 
-import {Component, HostBinding} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+import {SuiSize, Utils} from '../common';
+
+export type SuiFormState = 'success' | 'warning' | 'error' | null;
 
 @Component({
   selector: '[sui-form]',
@@ -11,8 +14,18 @@ import {Component, HostBinding} from '@angular/core';
   `
 })
 export class SuiFormComponent {
+  @Input() suiState: SuiFormState = null;
+  @Input() suiSize: SuiSize = null;
+  @Input() suiLoading = false;
+
   @HostBinding('class')
   get classes(): string {
-    return ['ui', 'form'].joinWithWhitespaceCleanup();
+    return [
+      'ui',
+      Utils.getPropClass(this.suiLoading, 'loading'),
+      this.suiSize,
+      'form',
+      this.suiState
+    ].joinWithWhitespaceCleanup();
   }
 }
