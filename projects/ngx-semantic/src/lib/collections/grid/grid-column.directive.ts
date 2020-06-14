f96b94b68c9413db719c4c0bd9a1024fb931e26e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-06-14 22:28:47 +0100

    ft: finalize the column directive

--- projects/ngx-semantic/src/lib/collections/grid/grid-column.directive.ts
+++ projects/ngx-semantic/src/lib/collections/grid/grid-column.directive.ts
@@ -5,16 +5,21 @@
 import {Directive, HostBinding, Input} from '@angular/core';
 import {SuiWidth} from '../../common';
 
+export type SuiColumnFloat = 'left floated' | 'right floated' | null;
+
 @Directive({
   selector: '[sui-grid-column]'
 })
 export class SuiGridColumnDirective {
   @Input() suiWidth: SuiWidth = null;
+  @Input() suiFloated: SuiColumnFloat = null;
 
   @HostBinding('class')
   get classes(): string {
     return [
+      this.suiFloated,
       this.suiWidth,
+      this.suiWidth ? 'wide' : '',
       'column'
     ].joinWithWhitespaceCleanup();
   }
