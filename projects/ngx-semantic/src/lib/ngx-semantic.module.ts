9311817cf272bc465fa6474e9d5a1a3f061ca1e4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-06-17 18:42:22 +0100

    ft: rework the imports

--- projects/ngx-semantic/src/lib/ngx-semantic.module.ts
+++ projects/ngx-semantic/src/lib/ngx-semantic.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {SuiButtonModule} from './elements/button';
 import {SuiDividerModule} from './elements/divider';
-import {SuiIconModule} from './elements/icon';
+import {SuiFlagModule} from './elements/flag';
 import {SuiHeaderModule} from './elements/header';
+import {SuiIconModule} from './elements/icon';
 import {SuiImageModule} from './elements/image';
 import {SuiInputModule} from './elements/input';
 import {SuiLabelModule} from './elements/label';
@@ -12,10 +13,10 @@ import {SuiPlaceholderModule} from './elements/placeholder';
 import {SuiRailModule} from './elements/rail';
 import {SuiRevealModule} from './elements/reveal';
 import {SuiSegmentModule} from './elements/segment';
-import {SuiFlagModule} from './elements/flag';
 import {SuiBreadcrumbModule} from './collections/breadcrumb';
 import {SuiFormModule} from './collections/form';
 import {SuiGridModule} from './collections/grid';
+import {SuiMenuModule} from './collections/menu';
 
 @NgModule({
   declarations: [],
@@ -37,7 +38,8 @@ import {SuiGridModule} from './collections/grid';
     SuiSegmentModule,
     SuiBreadcrumbModule,
     SuiFormModule,
-    SuiGridModule
+    SuiGridModule,
+    SuiMenuModule
   ]
 })
 export class NgxSemanticModule {
