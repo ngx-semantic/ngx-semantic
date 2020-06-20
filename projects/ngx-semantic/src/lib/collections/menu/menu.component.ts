962e03c02c26cc35314d2160244d815a99176c15
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-06-20 17:30:17 +0100

    ft: add size support and update attachment options

--- projects/ngx-semantic/src/lib/collections/menu/menu.component.ts
+++ projects/ngx-semantic/src/lib/collections/menu/menu.component.ts
@@ -3,10 +3,11 @@
  */
 
 import {Component, HostBinding, Input} from '@angular/core';
-import {SuiColour, SuiWidth, Utils} from '../../common';
+import {SuiColour, SuiSize, SuiWidth, Utils} from '../../common';
 
-export type SuiMenuAttachment = 'top attached' | 'attached' | 'bottom attached' | null;
+export type SuiMenuAttachment = 'top' | 'bottom' | null;
 export type SuiMenuFixation = 'top' | 'bottom' | 'left' | 'right' | null;
+export type SuiMenuIconType = 'icon' | 'labeled icon' | null;
 
 @Component({
   selector: '[sui-menu]',
@@ -19,6 +20,8 @@ export class SuiMenuComponent {
   @Input() suiAttached: SuiMenuAttachment = null;
   @Input() suiFixed: SuiMenuFixation = null;
   @Input() suiColour: SuiColour = null;
+  @Input() suiIcon: SuiMenuIconType = null;
+  @Input() suiSize: SuiSize = null;
   @Input() suiText = false;
   @Input() suiSecondary = false;
   @Input() suiPointing = false;
@@ -30,29 +33,30 @@ export class SuiMenuComponent {
   @Input() suiCompact = false;
   @Input() suiStackable = false;
   @Input() suiInverted = false;
-  @Input() suiIcon = false;
 
   @HostBinding('class')
   get classes(): string {
     return [
       'ui',
       this.suiColour,
+      this.suiSize,
       this.suiAttached,
+      this.suiAttached ? 'attached' : '',
       this.suiFixed,
       this.suiFixed ? 'fixed' : '',
       this.suiWidth,
       this.suiWidth ? 'item' : '',
+      this.suiIcon,
       Utils.getPropClass(this.suiInverted, 'inverted'),
-      Utils.getPropClass(this.suiVertical, 'vertical'),
+      Utils.getPropClass(this.suiCompact, 'compact'),
       Utils.getPropClass(this.suiFluid, 'fluid'),
+      Utils.getPropClass(this.suiVertical, 'vertical'),
       Utils.getPropClass(this.suiText, 'text'),
       Utils.getPropClass(this.suiSecondary, 'secondary'),
       Utils.getPropClass(this.suiPointing, 'pointing'),
       Utils.getPropClass(this.suiRight, 'right'),
       Utils.getPropClass(this.suiTabular, 'tabular'),
       Utils.getPropClass(this.suiPagination, 'pagination'),
-      Utils.getPropClass(this.suiCompact, 'compact'),
-      Utils.getPropClass(this.suiIcon, 'icon'),
       Utils.getPropClass(this.suiStackable, 'stackable'),
       'menu'
     ].joinWithWhitespaceCleanup();
