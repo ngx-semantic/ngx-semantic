302747f1c5a48e039a2c98bb04f16bb6e9567782
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-06-28 00:33:20 +0100

    ft: add in header support

--- projects/ngx-semantic/src/lib/collections/message/message.module.ts
+++ projects/ngx-semantic/src/lib/collections/message/message.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from "@angular/core";
 import {SuiMessageComponent} from "./message.component";
 import {CommonModule} from "@angular/common";
+import {SuiMessageHeaderDirective} from "./message-header.directive";
 
 @NgModule({
-  declarations: [SuiMessageComponent],
+  declarations: [SuiMessageComponent, SuiMessageHeaderDirective],
   imports: [CommonModule],
-  exports: [SuiMessageComponent]
+  exports: [SuiMessageComponent, SuiMessageHeaderDirective]
 })
 export class SuiMessageModule {
 }
