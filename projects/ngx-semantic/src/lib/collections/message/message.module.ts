9a170f03dd819f9a140e369e3b63e0022b0312e2
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-06-28 18:37:47 +0100

    ft: fix linting issues

--- projects/ngx-semantic/src/lib/collections/message/message.module.ts
+++ projects/ngx-semantic/src/lib/collections/message/message.module.ts
@@ -1,8 +1,8 @@
-import {NgModule} from "@angular/core";
-import {SuiMessageComponent} from "./message.component";
-import {CommonModule} from "@angular/common";
-import {SuiMessageHeaderDirective} from "./message-header.directive";
-import {SuiMessageContentDirective} from "./message-content.directive";
+import {NgModule} from '@angular/core';
+import {SuiMessageComponent} from './message.component';
+import {CommonModule} from '@angular/common';
+import {SuiMessageHeaderDirective} from './message-header.directive';
+import {SuiMessageContentDirective} from './message-content.directive';
 
 @NgModule({
   declarations: [SuiMessageComponent, SuiMessageHeaderDirective, SuiMessageContentDirective],
