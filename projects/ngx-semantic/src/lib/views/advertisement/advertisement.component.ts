9edc5d8fcf7f5c7425569d6fcaf5c13d39cdd272
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-09 22:31:10 +0100

    ft: add in the module

--- projects/ngx-semantic/src/lib/views/advertisement/advertisement.component.ts
+++ projects/ngx-semantic/src/lib/views/advertisement/advertisement.component.ts
@@ -1,3 +1,14 @@
 /**
  * Created by bolor on 7/9/2020
  */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[sui-advertisement]',
+  template: `
+    <ng-content></ng-content>
+  `
+})
+export class SuiAdvertisementComponent {
+}
--- projects/ngx-semantic/src/lib/views/advertisement/public-api.ts
+++ projects/ngx-semantic/src/lib/views/advertisement/public-api.ts
@@ -1,3 +1,6 @@
 /**
  * Created by bolor on 7/9/2020
  */
+
+export * from './advertisement.component';
+export * from './advertisement.module';
