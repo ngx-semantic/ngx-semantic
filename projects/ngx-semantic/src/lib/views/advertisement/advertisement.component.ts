f806b6b586cc807547501db8b30d364014b395be
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-09 22:50:04 +0100

    ft: add the ad component scaffold

--- projects/ngx-semantic/src/lib/views/advertisement/advertisement.component.ts
+++ projects/ngx-semantic/src/lib/views/advertisement/advertisement.component.ts
@@ -2,7 +2,21 @@
  * Created by bolor on 7/9/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+import {SuiSize, Utils} from '../../common';
+
+export type SuiAdType =
+  'banner'
+  | 'rectangle'
+  | 'leaderboard'
+  | 'page'
+  | 'square button'
+  | 'button'
+  | 'skyscraper'
+  | 'billboard'
+  | 'panorama'
+  | 'netboard'
+  | null;
 
 @Component({
   selector: '[sui-advertisement]',
@@ -11,4 +25,24 @@ import {Component} from '@angular/core';
   `
 })
 export class SuiAdvertisementComponent {
+  @Input() suiSize: SuiSize = null;
+  @Input() suiType: SuiAdType = null;
+  @Input() suiVertical = false;
+  @Input() suiHalf = false;
+  @Input() suiWide = false;
+  @Input() suiCentered = false;
+
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'ui',
+      this.suiSize,
+      Utils.getPropClass(this.suiCentered, 'centered'),
+      Utils.getPropClass(this.suiHalf, 'half'),
+      Utils.getPropClass(this.suiWide, 'wide'),
+      Utils.getPropClass(this.suiVertical, 'vertical'),
+      this.suiType,
+      'ad'
+    ].joinWithWhitespaceCleanup();
+  }
 }
