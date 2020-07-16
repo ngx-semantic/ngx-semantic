12678a13280ff7575a000f58c806b8e63022d6a6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-16 11:07:51 +0100

    ft: add in index and public api exports

--- projects/ngx-semantic/src/lib/views/statistics/index.ts
+++ projects/ngx-semantic/src/lib/views/statistics/index.ts
@@ -1,3 +1,5 @@
 /**
  * Created by bolor on 7/16/2020
  */
+
+export * from './public-api';
--- projects/ngx-semantic/src/lib/views/statistics/public-api.ts
+++ projects/ngx-semantic/src/lib/views/statistics/public-api.ts
@@ -1,3 +1,7 @@
 /**
  * Created by bolor on 7/16/2020
  */
+
+export * from './statistic-label.directive';
+export * from './statistic-value.directive';
+export * from './statistic.module';
