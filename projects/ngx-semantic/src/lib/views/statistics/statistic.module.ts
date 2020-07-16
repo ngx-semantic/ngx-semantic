abf0b19ad267dc641929fc8793b71ef71f97522e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-16 11:06:50 +0100

    ft: add in a fe exports on teh module

--- projects/ngx-semantic/src/lib/views/statistics/statistic.module.ts
+++ projects/ngx-semantic/src/lib/views/statistics/statistic.module.ts
@@ -1,3 +1,16 @@
 /**
  * Created by bolor on 7/16/2020
  */
+
+import {NgModule} from '@angular/core';
+import {SuiStatisticLabelDirective} from './statistic-label.directive';
+import {SuiStatisticValueDirective} from './statistic-value.directive';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [SuiStatisticLabelDirective, SuiStatisticValueDirective],
+  imports: [CommonModule],
+  exports: [SuiStatisticValueDirective, SuiStatisticLabelDirective]
+})
+export class SuiStatisticModule {
+}
