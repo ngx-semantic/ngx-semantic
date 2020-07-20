3e577cde78772dc31d49c9bc4c400153344f0350
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-20 14:10:20 +0100

    ft: finalize exports

--- projects/ngx-semantic/src/lib/views/comment/comment.module.ts
+++ projects/ngx-semantic/src/lib/views/comment/comment.module.ts
@@ -6,11 +6,32 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SuiCommentComponent} from './comment.component';
 import {SuiCommentsComponent} from './comments.component';
+import {SuiCommentActionsDirective} from './comment-actions.directive';
+import {SuiCommentAvatarDirective} from './comment-avatar.directive';
+import {SuiCommentMetadataDirective} from './comment-metadata.directive';
+import {SuiCommentContentDirective} from './comment-content.directive';
+import {SuiCommentTextDirective} from './comment-text.directive';
 
 @NgModule({
-  declarations: [SuiCommentComponent, SuiCommentsComponent],
+  declarations: [
+    SuiCommentComponent,
+    SuiCommentsComponent,
+    SuiCommentActionsDirective,
+    SuiCommentAvatarDirective,
+    SuiCommentMetadataDirective,
+    SuiCommentContentDirective,
+    SuiCommentTextDirective
+  ],
   imports: [CommonModule],
-  exports: [SuiCommentComponent, SuiCommentComponent]
+  exports: [
+    SuiCommentComponent,
+    SuiCommentComponent,
+    SuiCommentActionsDirective,
+    SuiCommentAvatarDirective,
+    SuiCommentMetadataDirective,
+    SuiCommentContentDirective,
+    SuiCommentTextDirective
+  ]
 })
 export class SuiCommentModule {
 }
--- projects/ngx-semantic/src/lib/views/comment/public-api.ts
+++ projects/ngx-semantic/src/lib/views/comment/public-api.ts
@@ -2,6 +2,11 @@
  * Created by bolor on 7/20/2020
  */
 
+export * from './comment-actions.directive';
+export * from './comment-avatar.directive';
+export * from './comment-metadata.directive';
+export * from './comment-text.directive';
+export * from './comment-content.directive';
 export * from './comment.component';
 export * from './comments.component';
 export * from './comment.module';
