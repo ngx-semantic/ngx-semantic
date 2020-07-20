3b1f33fde246c7531e5f73c67f259272752568f0
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-20 12:35:30 +0100

    ft: add in public api exports

--- projects/ngx-semantic/src/lib/views/comment/index.ts
+++ projects/ngx-semantic/src/lib/views/comment/index.ts
@@ -1,3 +1,5 @@
 /**
  * Created by bolor on 7/20/2020
  */
+
+export * from './public-api';
--- projects/ngx-semantic/src/lib/views/comment/public-api.ts
+++ projects/ngx-semantic/src/lib/views/comment/public-api.ts
@@ -1,3 +1,7 @@
 /**
  * Created by bolor on 7/20/2020
  */
+
+export * from './comment.component';
+export * from './comments.component';
+export * from './comment.module';
