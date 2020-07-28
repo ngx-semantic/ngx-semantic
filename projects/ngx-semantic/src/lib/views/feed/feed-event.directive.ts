1678c0f9e91dd5c4505ff7629bffd6f0d0048536
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-28 12:34:51 +0100

    ft: add in event directive

--- projects/ngx-semantic/src/lib/views/feed/feed-event.directive.ts
+++ projects/ngx-semantic/src/lib/views/feed/feed-event.directive.ts
@@ -2,10 +2,14 @@
  * Created by bolor on 7/28/2020
  */
 
-import {Directive} from '@angular/core';
+import {Directive, HostBinding} from '@angular/core';
 
 @Directive({
   selector: 'suiFeedEvent'
 })
 export class SuiFeedEventDirective {
+  @HostBinding('class')
+  get classes(): string {
+    return ['event'].joinWithWhitespaceCleanup();
+  }
 }
