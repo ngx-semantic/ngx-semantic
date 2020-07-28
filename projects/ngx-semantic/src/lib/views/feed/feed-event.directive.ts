23e8aa0b0f9e648cbff4cc58f598c1b5322f0e5a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-28 12:25:24 +0100

    ft: add in scaffolds

--- projects/ngx-semantic/src/lib/views/feed/feed-event.directive.ts
+++ projects/ngx-semantic/src/lib/views/feed/feed-event.directive.ts
@@ -1,3 +1,11 @@
 /**
  * Created by bolor on 7/28/2020
  */
+
+import {Directive} from '@angular/core';
+
+@Directive({
+  selector: 'suiFeedEvent'
+})
+export class SuiFeedEventDirective {
+}
--- projects/ngx-semantic/src/lib/views/feed/feed.component.ts
+++ projects/ngx-semantic/src/lib/views/feed/feed.component.ts
@@ -1,3 +1,14 @@
 /**
  * Created by bolor on 7/28/2020
  */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[sui-feed]',
+  template: `
+    <ng-content></ng-content>
+  `
+})
+export class SuiFeedComponent {
+}
