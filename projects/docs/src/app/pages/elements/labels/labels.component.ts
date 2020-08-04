67520a8062083df36d1050da865ad7c1bf46ef17
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-08-04 15:12:22 +0100

    label snippets

--- projects/docs/src/app/pages/elements/labels/labels.component.ts
+++ projects/docs/src/app/pages/elements/labels/labels.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./labels.component.scss']
 })
 export class LabelsComponent implements OnInit {
+  toggleStateMap: any = {};
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toggleLabel(key: string) {
+    this.toggleStateMap[key] = !this.toggleStateMap[key];
+  }
+
+
+  snippetColored = `
+  <a sui-label suiColour="red">Red</a>
+  <a sui-label suiColour="orange">Orange</a>
+  <a sui-label suiColour="yellow">Yellow</a>
+  <a sui-label suiColour="olive">Olive</a>
+  `;
+
+  snippetCircular = `
+  <a sui-label suiColour="red" suiCircular="true">2</a>
+  <a sui-label suiColour="orange" suiCircular="true">2</a>
+  <a sui-label suiColour="yellow" suiCircular="true">2</a>
+  <a sui-label suiColour="olive" suiCircular="true">2</a>
+  `;
 
 }
