d01f082c9ad176109e8e10a3ed765d323b95b2bc
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-08-04 14:17:03 +0100

    input snippets

--- projects/docs/src/app/pages/elements/inputs/inputs.component.ts
+++ projects/docs/src/app/pages/elements/inputs/inputs.component.ts
@@ -6,9 +6,39 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./inputs.component.scss']
 })
 export class InputsComponent implements OnInit {
+  toggleStateMap: any = {};
+
   constructor() {
   }
 
   ngOnInit(): void {
   }
+
+  toggleLabel(key: string) {
+    this.toggleStateMap[key] = !this.toggleStateMap[key];
+  }
+
+  snippetInput = `
+  <div sui-input>
+    <input type="text" placeholder="Search...">
+  </div>
+  `;
+
+  snippetAction = `
+   <div sui-input suiAction="left action">
+     <button sui-button suiColour="teal" suiLabeled="labeled" suiIcon="true">
+       <i sui-icon suiIconType="cart"></i>
+       Checkout
+     </button>
+     <input type="text" value="$52.03">
+   </div>
+   
+   <div sui-input suiAction="right action">
+     <input type="text" value="http://www.short.url/c0opq">
+     <button sui-button suiColour="teal" suiLocation="right" suiLabeled="true" suiIcon="true">
+       <i sui-icon suiIconType="copy"></i>
+       Copy
+     </button>
+   </div>
+  `;
 }
