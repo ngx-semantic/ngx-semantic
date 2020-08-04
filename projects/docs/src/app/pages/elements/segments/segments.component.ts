d22dc726f742fb57092850aec951dffbe9b314d7
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-08-04 16:39:31 +0100

    segment snippets

--- projects/docs/src/app/pages/elements/segments/segments.component.ts
+++ projects/docs/src/app/pages/elements/segments/segments.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./segments.component.scss']
 })
 export class SegmentsComponent implements OnInit {
+  toggleStateMap: any = {};
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toggleLabel(key: string) {
+    this.toggleStateMap[key] = !this.toggleStateMap[key];
+  }
+
+  snippetSegment = `
+  <div sui-segment suiColour="red" [suiInverted]="true">Red</div>
+  <div sui-segment suiColour="orange" [suiInverted]="true">Orange</div>
+  <div sui-segment suiColour="yellow" [suiInverted]="true">Yellow</div>
+  <div sui-segment suiColour="olive" [suiInverted]="true">Olive</div>
+  <div sui-segment suiColour="green" [suiInverted]="true">Green</div>`;
 
 }
