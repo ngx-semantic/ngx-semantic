b0721d798b1d3fa675dba37e4474bfb4a5555b8f
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-08-04 15:37:11 +0100

    loaders snippets

--- projects/docs/src/app/pages/elements/loaders/loaders.component.ts
+++ projects/docs/src/app/pages/elements/loaders/loaders.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loaders.component.scss']
 })
 export class LoadersComponent implements OnInit {
+  toggleStateMap: any = {};
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toggleLabel(key: string) {
+    this.toggleStateMap[key] = !this.toggleStateMap[key];
+  }
+
+  snippetStdLoader = ` <div sui-loader suiSize="medium" suiText="false"></div>`;
+
+  snippetTextLoader = ` <div sui-loader suiSize="medium" suiText="true">Loading</div>`;
+
+  snippetSizedLoaders = `
+  <div sui-loader suiSize="mini" suiText="true">Loading</div>
+  <div sui-loader suiSize="tiny" suiText="true">Loading</div>
+  <div sui-loader suiSize="small" suiText="true">Loading</div>
+  <div sui-loader suiSize="medium" suiText="true">Loading</div>
+  <div sui-loader suiSize="large" suiText="true">Loading</div>
+  <div sui-loader suiSize="big" suiText="true">Loading</div>
+  <div sui-loader suiSize="huge" suiText="true">Loading</div>`;
 
 }
