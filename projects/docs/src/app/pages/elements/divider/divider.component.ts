ab86b1d59271784e27dbe2d7b3c2a4231be7cdab
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-08-04 13:48:44 +0100

    divider snippets

--- projects/docs/src/app/pages/elements/divider/divider.component.ts
+++ projects/docs/src/app/pages/elements/divider/divider.component.ts
@@ -18,4 +18,62 @@ export class DividerComponent implements OnInit {
     this.toggleStateMap[key] = !this.toggleStateMap[key];
   }
 
+  stdDivider = `
+  <div>
+    <p> Some content here ...</p>
+  </div>
+
+  <div sui-divider></div>
+
+  <div>
+    <p> Some content here ...</p>
+  </div>`;
+
+  verticalDivider = `
+  <div sui-segment>
+    <div class="ui two column very relaxed grid">
+      <div class="column">
+        <div>
+          <p> Some content ...</p>
+        </div>
+        <div>
+          <p> Some content ...</p>
+        </div>
+      </div>
+
+      <div class="column">
+        <div>
+          <p> Some content ...</p>
+        </div>
+        <div>
+          <p> Some content ...</p>
+        </div>
+      </div>
+    </div>
+    <div sui-divider suiDirection="vertical">
+      and
+    </div>
+  </div>
+      `;
+
+  horizontalDivider = `
+  <div sui-segment suiTextAlignment="center aligned" suiBasic="true">
+    <div sui-input suiAction="left action" suiIcon="true">
+      <input type="text" placeholder="Order #">
+      <div sui-button suiColour="blue">
+        Search
+      </div>
+    </div>
+
+    <div sui-divider suiDirection="horizontal">
+        Or
+    </div>
+
+    <div class="ui teal labeled icon button">
+      Create New Order
+      <i class="add icon"></i>
+    </div>
+  </div>
+  `;
+
 }
