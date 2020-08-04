fedd3f71fa6278f833abb5231eefea49aa77a87a
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-08-04 16:46:12 +0100

    reveal snippet

--- projects/docs/src/app/pages/elements/reveals/reveals.component.ts
+++ projects/docs/src/app/pages/elements/reveals/reveals.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reveals.component.scss']
 })
 export class RevealsComponent implements OnInit {
+  toggleStateMap: any = {};
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toggleLabel(key: string) {
+    this.toggleStateMap[key] = !this.toggleStateMap[key];
+  }
+
+  snippetMoveReveal = `
+  <div sui-reveal suiSize="small" suiMove="right" suiImage="true">
+    <img sui-reveal-content suiVisible="visible" src="https://semantic-ui.com/images/avatar/large/ade.jpg">
+    <img sui-reveal-content suiVisible="hidden" src="https://semantic-ui.com/images/wireframe/square-image.png">
+  </div>
+
+  <div sui-reveal suiSize="medium" suiRotate="right" suiImage="true">
+    <img sui-reveal-content suiVisible="visible" src="https://semantic-ui.com/images/avatar/large/ade.jpg">
+    <img sui-reveal-content suiVisible="hidden" src="https://semantic-ui.com/images/wireframe/square-image.png">
+  </div>
+  `;
 
 }
