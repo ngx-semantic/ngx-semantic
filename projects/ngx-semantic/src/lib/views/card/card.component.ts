a827ac117f82b5c88e000c1befdf37d062e89359
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-08-17 03:21:26 +0100

    ft: add card special fields

--- projects/ngx-semantic/src/lib/views/card/card.component.ts
+++ projects/ngx-semantic/src/lib/views/card/card.component.ts
@@ -2,7 +2,8 @@
  * Created by bolor on 8/17/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+import {SuiColour, Utils} from '../../common';
 
 @Component({
   selector: '[sui-card]',
@@ -11,4 +12,20 @@ import {Component} from '@angular/core';
   `
 })
 export class SuiCardComponent {
+  @Input() suiColour: SuiColour = null;
+  @Input() suiLink = false;
+  @Input() suiCentered = false;
+  @Input() suiFluid = false;
+
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'ui',
+      this.suiColour,
+      Utils.getPropClass(this.suiLink, 'link'),
+      Utils.getPropClass(this.suiCentered, 'centered'),
+      Utils.getPropClass(this.suiFluid, 'fluid'),
+      'card'
+    ].joinWithWhitespaceCleanup();
+  }
 }
