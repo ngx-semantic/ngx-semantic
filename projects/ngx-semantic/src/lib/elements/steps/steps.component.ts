25c7dff1054165d59e38157647d6d5563a2c608d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-21 09:46:36 +0100

    ft: add size and unstackable support

--- projects/ngx-semantic/src/lib/elements/steps/steps.component.ts
+++ projects/ngx-semantic/src/lib/elements/steps/steps.component.ts
@@ -3,7 +3,9 @@
  */
 
 import {Component, HostBinding, Input} from '@angular/core';
-import {SuiWidth, Utils} from '../../common';
+import {SuiSize, SuiWidth, Utils} from '../../common';
+
+export type SuiStepsAttachment = 'top attached' | 'bottom attached' | null;
 
 @Component({
   selector: '[sui-steps]',
@@ -13,18 +15,24 @@ import {SuiWidth, Utils} from '../../common';
 })
 export class SuiStepsComponent {
   @Input() suiWidth: SuiWidth = null;
+  @Input() suiAttached: SuiStepsAttachment = null;
+  @Input() suiSize: SuiSize = null;
   @Input() suiOrdered = false;
   @Input() suiVertical = false;
   @Input() suiFluid = false;
+  @Input() suiUnstackable = false;
 
   @HostBinding('class')
   get classes(): string {
     return [
       'ui',
+      this.suiSize,
       this.suiWidth,
+      this.suiAttached,
       Utils.getPropClass(this.suiOrdered, 'ordered'),
       Utils.getPropClass(this.suiFluid, 'fluid'),
       Utils.getPropClass(this.suiVertical, 'vertical'),
+      Utils.getPropClass(this.suiUnstackable, 'unstackable'),
       'steps'
     ].joinWithWhitespaceCleanup();
   }
