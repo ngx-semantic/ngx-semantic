984120700953867e37ad2095be1d8475c472d81f
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-09-21 11:43:05 +0100

    Setup Documentaion for Messages

--- projects/docs/src/app/pages/collections/messages/messages.component.ts
+++ projects/docs/src/app/pages/collections/messages/messages.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent implements OnInit {
+  snippetBasic = `
+    <div sui-message suiColour="blue" suiSize="large">
+      <div sui-message-header>
+        Welcome back!
+      </div>
+      <p>A basic message.</p>
+    </div>
+  `;
+  snippetDismissable = `
+  <div sui-message [suiDismissable]="true">
+    <div sui-message-header>
+      Welcome back!
+    </div>
+    <p>This is a special notification which you can dismiss if you're bored with it.</p>
+  </div>
+  `;
 
   constructor() { }
 
+  toggleStateMap: any = {};
+
   ngOnInit(): void {
+  }
+
+  toggleLabel(key: string) {
+    this.toggleStateMap[key] = !this.toggleStateMap[key];
   }
 
 }
