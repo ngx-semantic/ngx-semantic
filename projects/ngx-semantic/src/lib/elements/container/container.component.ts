ea20e0f6a48bde405ef82530a10118de2d085dac
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-22 11:26:42 +0100

    ft: finalize container support

--- projects/ngx-semantic/src/lib/elements/container/container.component.ts
+++ projects/ngx-semantic/src/lib/elements/container/container.component.ts
@@ -2,7 +2,10 @@
  * Created by bolor on 9/22/2020
  */
 
-import {Component, HostBinding} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+import {Utils} from '../../common';
+
+export type SuiContainerAlignment = 'left aligned' | 'right aligned' | 'center aligned' | 'justified' | null;
 
 @Component({
   selector: '[sui-container]',
@@ -11,10 +14,17 @@ import {Component, HostBinding} from '@angular/core';
   `
 })
 export class SuiContainerComponent {
+  @Input() suiAlignment: SuiContainerAlignment = null;
+  @Input() suiText = false;
+  @Input() suiFluid = false;
+
   @HostBinding('class')
   get classes(): string {
     return [
       'ui',
+      this.suiAlignment,
+      Utils.getPropClass(this.suiText, 'text'),
+      Utils.getPropClass(this.suiFluid, 'fluid'),
       'container'
     ].joinWithWhitespaceCleanup();
   }
