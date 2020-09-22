b192f086a138b44e092a8c5411b2858c65be3043
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-09-22 11:15:16 +0100

    ft: add in container scaffolds

---
+++ projects/ngx-semantic/src/lib/elements/container/container.component.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by bolor on 9/22/2020
+ */
---
+++ projects/ngx-semantic/src/lib/elements/container/container.module.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by bolor on 9/22/2020
+ */
+
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: []
+})
+export class SuiContainerModule {
+}
