bea2dbb01b91a2dc9ef0475f33077ec3db6785d4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-02 20:23:16 +0100

    ft: take first step to display rendered code

--- projects/docs/src/app/components/code-sample.component.ts
+++ projects/docs/src/app/components/code-sample.component.ts
@@ -1,17 +1,43 @@
-import {Component} from '@angular/core';
+import {Component, Input, TemplateRef} from '@angular/core';
 
 @Component({
   selector: 'doc-code-sample',
   template: `
-    <div>
+    <div class="toggle-row">
       <i sui-icon
-         [suiIconType]="code"></i>
+         suiIconType="code"
+         title="Toggle Code"
+         style="cursor: pointer"></i>
     </div>
 
     <ng-content></ng-content>
-    <div>code sample</div>
-  `
+
+    <div sui-segment
+         suiAttached="top attached"
+         style="padding-top: 3rem;">
+      <ng-container *ngTemplateOutlet="content"></ng-container>
+      <div sui-label
+           suiAttached="top">
+        Example
+        <i sui-icon
+           suiIconType="copy"
+           title="Copy Code"
+           style="cursor: pointer; float: right;"></i>
+      </div>
+    </div>
+    <div sui-segment
+         suiAttached="bottom attached">
+      <div>code content</div>
+    </div>
+  `,
+  styles: [`
+    .toggle-row {
+      display: flex;
+      flex-direction: row-reverse;
+      margin-bottom: 1rem;
+    }
+  `]
 })
 export class CodeSampleComponent {
-
+  @Input() content: TemplateRef<any>;
 }
