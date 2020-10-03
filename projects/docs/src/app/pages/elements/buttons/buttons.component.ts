e7a180a5fceffacc605992402ccf3374db811f77
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-03 15:57:18 +0100

    ft: add initial labeled support

--- projects/docs/src/app/pages/elements/buttons/buttons.component.ts
+++ projects/docs/src/app/pages/elements/buttons/buttons.component.ts
@@ -30,6 +30,80 @@ export class ButtonsComponent implements OnInit {
     Discard
   </button>`;
 
+  snippetBtnSecondaryEmphasis = `
+  <button sui-button suiEmphasis="secondary">
+    Save
+  </button>
+
+  <button sui-button>
+    Discard
+  </button>`;
+
+  snippetBtnAnimated = `
+  <div sui-button
+     tabindex="0"
+     suiAnimated="animated">
+    <div class="visible content">Next</div>
+    <div class="hidden content">
+      <i sui-icon
+         suiIconType="right arrow"></i>
+    </div>
+  </div>
+  <div sui-button
+       tabindex="0"
+       suiAnimated="vertical animated">
+    <div class="hidden content">Shop</div>
+    <div class="visible content">
+      <i class="shop icon"></i>
+    </div>
+  </div>
+  <div sui-button
+       tabindex="0"
+       suiAnimated="animated fade">
+    <div class="visible content">Sign-up for a Pro account</div>
+    <div class="hidden content">
+      $12.99 a month
+    </div>
+  </div>`;
+
+  snippetBtnLabeled = `
+  <div sui-button
+       tabindex="0"
+       suiLabeled="labeled">
+    <div sui-button>
+      <i sui-icon
+         suiIconType="heart"></i> Like
+    </div>
+    <a sui-label
+       suiBasic="true">
+      2,048
+    </a>
+  </div>
+  <div sui-button
+       suiLabeled="left labeled"
+       tabindex="0">
+    <a sui-label
+       suiBasic="true"
+       suiPointing="right">
+      2,048
+    </a>
+    <div sui-button>
+      <i sui-icon
+         suiIconType="heart"></i> Like
+    </div>
+  </div>
+  <div class="ui left labeled button" tabindex="0">
+    <a sui-label
+       suiBasic="true">
+      1,048
+    </a>
+    <div sui-button
+         suiIcon="true">
+      <i sui-icon
+         suiIconType="fork"></i>
+    </div>
+  </div>`;
+
   snippetBtnBasic = `
   <button sui-button suiBasic="true" suiColour="red" suiSize="medium">
     Red
