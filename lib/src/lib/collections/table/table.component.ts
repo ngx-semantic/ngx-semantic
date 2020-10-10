079a3f18580d650f1e7661b53b800a24bd2660d9
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-10 18:42:24 +0100

    #56 | add component and module scaffold

--- lib/src/lib/collections/table/table.component.ts
+++ lib/src/lib/collections/table/table.component.ts
@@ -1,3 +1,14 @@
 /**
  * Created by bolor on 10/10/2020
  */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '[sui-table]',
+  template: `
+    <ng-content></ng-content>
+  `,
+})
+export class SuiTableComponent {
+}
--- lib/src/lib/collections/table/table.module.ts
+++ lib/src/lib/collections/table/table.module.ts
@@ -1,3 +1,21 @@
 /**
  * Created by bolor on 10/10/2020
  */
+
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {SuiTableComponent} from './table.component';
+
+@NgModule({
+  declarations: [
+    SuiTableComponent,
+  ],
+  exports: [
+    SuiTableComponent,
+  ],
+  imports: [
+    CommonModule,
+  ],
+})
+export class SuiTableModule {
+}
