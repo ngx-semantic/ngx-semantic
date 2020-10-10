19d175ee16d941e6b42505c322775b4c27a860b3
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-10 18:39:46 +0100

    #56 | export stuff

--- lib/src/lib/collections/table/index.ts
+++ lib/src/lib/collections/table/index.ts
@@ -1,3 +1,5 @@
 /**
  * Created by bolor on 10/10/2020
  */
+
+export * from './public-api';
--- lib/src/lib/collections/table/public-api.ts
+++ lib/src/lib/collections/table/public-api.ts
@@ -1,3 +1,6 @@
 /**
  * Created by bolor on 10/10/2020
  */
+
+export * from './table.component';
+export * from './table.module';
