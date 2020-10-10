53b838bc1e3a286cd12f29ef91e1ed7ef824e76d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-10 19:56:35 +0100

    #56 | add support for other table styles

--- lib/src/lib/collections/table/table.component.ts
+++ lib/src/lib/collections/table/table.component.ts
@@ -15,14 +15,20 @@ export class SuiTableComponent {
   @Input() public suiCelled = false;
   @Input() public suiPadded = false;
   @Input() public suiStriped = false;
+  @Input() public suiDefinition = false;
+  @Input() public suiCompact = false;
+  @Input() public suiStructured = false;
 
   @HostBinding('class')
   get classes(): string {
     return [
       'ui',
+      Utils.getPropClass(this.suiCompact, 'compact'),
       Utils.getPropClass(this.suiCelled, 'celled'),
       Utils.getPropClass(this.suiPadded, 'padded'),
       Utils.getPropClass(this.suiStriped, 'striped'),
+      Utils.getPropClass(this.suiDefinition, 'definition'),
+      Utils.getPropClass(this.suiStructured, 'structured'),
       'table'
     ].joinWithWhitespaceCleanup();
   }
