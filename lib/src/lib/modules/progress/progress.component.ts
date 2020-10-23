5c38b3334e07feaffeee12679cf4367d9d6166a8
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-23 14:37:05 +0100

    #65 | add in progress details

--- lib/src/lib/modules/progress/progress.component.ts
+++ lib/src/lib/modules/progress/progress.component.ts
@@ -2,13 +2,94 @@
  * Created by bolor on 10/22/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+import {SuiColour, SuiSize, Utils} from '../../common';
+
+export type SuiProgressAttachment = 'bottom' | 'top' | null;
+export type SuiProgressState = 'success' | 'warning' | 'error' | null;
 
 @Component({
   selector: 'sui-progress',
   template: `
-    <div></div>
+    <div class="bar"
+         [style.width.%]="progressPercentage">
+      <ng-container *ngIf="suiShowProgress">
+        <div class="progress">{{progressPercentage}}%</div>
+      </ng-container>
+    </div>
+    <div class="label">
+      <ng-content></ng-content>
+    </div>
   `,
+  styles: [`
+    :host {
+      width: 100%;
+    }
+  `],
 })
 export class SuiProgressComponent {
+  @Input() public suiAttached: SuiProgressAttachment = null;
+  @Input() public suiSize: SuiSize = null;
+  @Input() public suiColour: SuiColour = null;
+  @Input() public suiState: SuiProgressState = null;
+  @Input() public suiIndicating = false;
+  @Input() public suiDisabled = false;
+  @Input() public suiInverted = false;
+  @Input() public suiShowProgress = true;
+
+  private value = 0;
+
+  @Input()
+  set suiValue(value: number) {
+    this.value = +value;
+    this.calculatePercentage();
+  }
+
+  get suiValue(): number {
+    return this.value;
+  }
+
+  private maxValue = 1000;
+
+  @Input()
+  set suiMaxValue(maxValue: number) {
+    this.maxValue = +maxValue;
+    this.calculatePercentage();
+  }
+
+  get suiMaxValue(): number {
+    return this.maxValue;
+  }
+
+  public progressPercentage: number;
+  public isActive: boolean;
+
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'ui',
+      this.suiSize,
+      this.suiColour,
+      this.suiAttached ? `${this.suiAttached} attached` : '',
+      Utils.getPropClass(this.isActive, 'active'),
+      Utils.getPropClass(this.suiIndicating, 'indicating'),
+      Utils.getPropClass(this.suiDisabled, 'disabled'),
+      Utils.getPropClass(this.suiInverted, 'inverted'),
+      'progress',
+      this.suiState,
+    ].joinWithWhitespaceCleanup();
+  }
+
+  private calculatePercentage(): void {
+    if (this.value > this.maxValue) {
+      return;
+    }
+
+    this.progressPercentage = Math.ceil(this.value * 100 / this.maxValue);
+    this.isActive = this.progressPercentage < 100;
+
+    if (this.progressPercentage === 100) {
+      this.suiState = 'success';
+    }
+  }
 }
