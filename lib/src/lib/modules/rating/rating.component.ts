bed776c4b4e687423dd15cc9b96a027dbbf3adc3
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-24 14:24:18 +0100

    #66 | add in component fields

--- lib/src/lib/modules/rating/rating.component.ts
+++ lib/src/lib/modules/rating/rating.component.ts
@@ -2,7 +2,10 @@
  * Created by bolor on 10/24/2020
  */
 
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {SuiSize} from '../../common';
+
+export type SuiRatingType = 'star' | 'heart' | null;
 
 @Component({
   selector: 'sui-rating',
@@ -10,4 +13,28 @@ import {Component} from '@angular/core';
   `
 })
 export class SuiRatingComponent {
+  @Input() public suiSize: SuiSize = null;
+  @Input() public suiType: SuiRatingType = null;
+
+  private value = 0;
+
+  @Input()
+  set suiValue(value: number) {
+    this.value = +value;
+  }
+
+  get suiValue(): number {
+    return this.value;
+  }
+
+  private maxValue = 1000;
+
+  @Input()
+  set suiMaxValue(maxValue: number) {
+    this.maxValue = +maxValue;
+  }
+
+  get suiMaxValue(): number {
+    return this.maxValue;
+  }
 }
