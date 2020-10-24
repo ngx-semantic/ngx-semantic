e9ac069b09080983ab80eae592ce936239fd71b6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-24 14:20:08 +0100

    #66 | add in module and rating scaffold

--- lib/src/lib/modules/rating/rating.module.ts
+++ lib/src/lib/modules/rating/rating.module.ts
@@ -1,3 +1,21 @@
 /**
  * Created by bolor on 10/24/2020
  */
+
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {SuiRatingComponent} from './rating.component';
+
+@NgModule({
+  declarations: [
+    SuiRatingComponent,
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    SuiRatingComponent,
+  ],
+})
+export class SuiRatingModule {
+}
