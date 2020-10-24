35933381730495c3eb8c984bd130ae9f47ea9b33
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-24 15:57:10 +0100

    #66 | add in support for clicks on the rating

--- lib/src/lib/modules/rating/rating.component.ts
+++ lib/src/lib/modules/rating/rating.component.ts
@@ -2,7 +2,7 @@
  * Created by bolor on 10/24/2020
  */
 
-import {Component, HostBinding, Input} from '@angular/core';
+import {Component, EventEmitter, HostBinding, Input, Output} from '@angular/core';
 import {SuiSize, Utils} from '../../common';
 
 export type SuiRatingType = 'star' | 'heart' | null;
@@ -10,8 +10,10 @@ export type SuiRatingType = 'star' | 'heart' | null;
 @Component({
   selector: 'sui-rating',
   template: `
-    <ng-container *ngFor="let rating of ratingsArray">
-      <i class="icon"></i>
+    <ng-container *ngFor="let i of ratingsArray">
+      <i class="icon"
+         [class.active]="i <= suiValue"
+         (click)="onClick(i)"></i>
     </ng-container>
   `,
   styles: [`
@@ -21,6 +23,7 @@ export type SuiRatingType = 'star' | 'heart' | null;
   `],
 })
 export class SuiRatingComponent {
+  @Output() public valueChanged = new EventEmitter();
   @Input() public suiSize: SuiSize = null;
   @Input() public suiType: SuiRatingType = null;
   @Input() public suiReadOnly = false;
@@ -57,6 +60,14 @@ export class SuiRatingComponent {
       Utils.getPropClass(this.suiReadOnly, 'read-only'),
       'rating',
     ].joinWithWhitespaceCleanup();
+  }
+
+  public onClick(value): void {
+    if (this.suiValue !== value) {
+      this.valueChanged.emit(value);
+    }
+
+    this.suiValue = value;
   }
 
   private generateRatingsArray(): void {
