e66bcaab7cb9986e632ea1f1d3768df7ecfa3268
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-30 14:51:12 +0100

    #61 | add basic drop down menu directive impl

--- lib/src/lib/modules/dropdown/dropdown-menu.directive.ts
+++ lib/src/lib/modules/dropdown/dropdown-menu.directive.ts
@@ -2,10 +2,16 @@
  * Created by bolor on 10/30/2020
  */
 
-import {Directive} from '@angular/core';
+import {Directive, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[suiDropdownMenu]'
 })
 export class SuiDropdownMenuDirective {
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'menu'
+    ].joinWithWhitespaceCleanup();
+  }
 }
