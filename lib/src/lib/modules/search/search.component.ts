ea0aa3b451a5b446baa1f0bea5804896ef2cf7a1
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-12-13 18:57:49 +0100

    #67 | add in module and component scaffolds

--- lib/src/lib/modules/search/search.component.ts
+++ lib/src/lib/modules/search/search.component.ts
@@ -1,1 +1,10 @@
-export * from './public-api';
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sui-search',
+  template: `
+    <ng-content></ng-content>
+  `
+})
+export class SuiSearchComponent {
+}
