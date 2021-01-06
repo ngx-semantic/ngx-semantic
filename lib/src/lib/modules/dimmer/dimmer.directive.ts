1d654b1b43b66791d57c529a9fd4a75cc64118d7
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-01-06 10:35:24 +0100

    #60 | support setting parent classes

--- lib/src/lib/modules/dimmer/dimmer.directive.ts
+++ lib/src/lib/modules/dimmer/dimmer.directive.ts
@@ -2,11 +2,35 @@
  * Created by bolorundurowb on 1/6/2021
  */
 
-import {Directive} from "@angular/core";
+import {Directive, EventEmitter, HostBinding, Input, Output} from '@angular/core';
+import {Utils} from '../../common';
 
 @Directive({
   selector: '[sui-dimmer]'
 })
 export class SuiDimmerDirective {
+  @Output() public dimmedChanged = new EventEmitter<boolean>();
 
+  // tslint:disable-next-line:variable-name
+  private _dimmed = false;
+
+  @Input()
+  public set dimmed(isDimmed: boolean) {
+    if (isDimmed !== this._dimmed) {
+      this._dimmed = isDimmed;
+      this.dimmedChanged.emit(this._dimmed);
+    }
+  }
+
+  public get dimmed(): boolean {
+    return this._dimmed;
+  }
+
+  @HostBinding('class')
+  get classes(): string {
+    return [
+      'dimmable',
+      Utils.getPropClass(this.dimmed, 'dimmed')
+    ].joinWithWhitespaceCleanup();
+  }
 }
