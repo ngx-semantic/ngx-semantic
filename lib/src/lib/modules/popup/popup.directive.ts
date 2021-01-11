acea198efff45664251e8eed683f3c0e5a622e01
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-01-11 12:15:40 +0100

    #64 | add in popup variants configs

--- lib/src/lib/modules/popup/popup.directive.ts
+++ lib/src/lib/modules/popup/popup.directive.ts
@@ -1,9 +1,27 @@
-import {Directive, Input} from '@angular/core';
+import {Directive, Input, TemplateRef} from '@angular/core';
+import {SuiSize} from '../../common';
+
+export type SuiPopupPlacement =
+  'top left'
+  | 'top center'
+  | 'top right'
+  | 'bottom left'
+  | 'bottom center'
+  | 'bottom right'
+  | 'right center'
+  | 'left center';
+export type SuiPopupWidth = 'wide' | 'very wide' | null;
 
 @Directive({
   selector: '[sui-popup]'
 })
 export class SuiPopupDirective {
+  @Input() public suiPopupPlacement: SuiPopupPlacement = 'top center';
+  @Input() public suiPopupWidth: SuiPopupWidth = null;
+  @Input() public suiPopupSize: SuiSize = null;
   @Input() public suiPopupTitle: string;
-  @Input() public suiPopupContent: string;
+  @Input() public suiPopupContent: string | TemplateRef<any>;
+  @Input() public suiPopupInverted = false;
+  @Input() public suiPopupFluid = false;
+  @Input() public suiPopupFlowing = false;
 }
