7c26472b218d6a975c95d68c8c604b63bd4d30eb
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-01-22 22:22:10 +0100

    #63 | add directives

--- lib/src/lib/modules/modal/modal-content.directive.ts
+++ lib/src/lib/modules/modal/modal-content.directive.ts
@@ -2,3 +2,11 @@
  * Created by bolorundurowb on 1/22/2021
  */
 
+import {Directive} from '@angular/core';
+
+@Directive({
+  selector: '[suiModalContent]',
+  exportAs: 'suiModalContent'
+})
+export class SuiModalContentDirective {
+}
--- lib/src/lib/modules/modal/modal-header.directive.ts
+++ lib/src/lib/modules/modal/modal-header.directive.ts
@@ -2,3 +2,11 @@
  * Created by bolorundurowb on 1/22/2021
  */
 
+import {Directive} from '@angular/core';
+
+@Directive({
+  selector: '[suiModalHeader]',
+  exportAs: 'suiModalHeader'
+})
+export class SuiModalHeaderDirective {
+}
