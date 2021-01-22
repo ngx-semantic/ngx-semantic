ee9cd42b4da988a8cf7771e49cb86e985840377f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-01-22 22:11:58 +0100

    #63 | add exports

--- lib/src/lib/modules/modal/index.ts
+++ lib/src/lib/modules/modal/index.ts
@@ -2,3 +2,4 @@
  * Created by bolorundurowb on 1/22/2021
  */
 
+export * from './public-api';
--- lib/src/lib/modules/modal/public-api.ts
+++ lib/src/lib/modules/modal/public-api.ts
@@ -2,3 +2,5 @@
  * Created by bolorundurowb on 1/22/2021
  */
 
+export * from './modal.component';
+export * from './modal.module';
