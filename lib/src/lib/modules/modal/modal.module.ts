e8fb343db859c2fcf79538c6101d601468efd4de
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-01-22 22:14:16 +0100

    #63 | add modal module implementation

--- lib/src/lib/modules/modal/modal.module.ts
+++ lib/src/lib/modules/modal/modal.module.ts
@@ -2,3 +2,22 @@
  * Created by bolorundurowb on 1/22/2021
  */
 
+import {OverlayModule} from '@angular/cdk/overlay';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {SuiModalComponent} from './modal.component';
+
+@NgModule({
+  exports: [
+    SuiModalComponent
+  ],
+  imports: [
+    CommonModule,
+    OverlayModule
+  ],
+  declarations: [
+    SuiModalComponent
+  ]
+})
+export class SuiModalModule {
+}
