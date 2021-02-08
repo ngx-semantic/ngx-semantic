5e27924592a49368b4a6a6e47a308faaec152508
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-08 15:33:33 +0100

    #106 | add link icon support

--- lib/src/lib/elements/icon/icon.directive.ts
+++ lib/src/lib/elements/icon/icon.directive.ts
@@ -1,5 +1,5 @@
 import {Directive, HostBinding, Input} from '@angular/core';
-import {Utils} from '../../common';
+import {SuiSize, Utils} from '../../common';
 import {InputBoolean} from '../../core/util';
 
 export type SuiIconFloatDirection = 'right floated' | null;
@@ -10,19 +10,23 @@ export type SuiIconFloatDirection = 'right floated' | null;
 })
 export class SuiIconDirective {
   @Input() public suiFloat: SuiIconFloatDirection = null;
+  @Input() public suiSize: SuiSize = null;
   @Input() public suiIconType: string = null;
   @Input() @InputBoolean() public disabled = false;
   @Input() @InputBoolean() public suiLoading = false;
   @Input() @InputBoolean() public suiFitted = false;
+  @Input() @InputBoolean() public suiLink = false;
 
   @HostBinding('class')
   get classes(): string {
     return [
       Utils.getPropClass(this.suiFitted, 'fitted'),
       Utils.getPropClass(this.disabled, 'disabled'),
+      this.suiSize,
       this.suiIconType,
       this.suiFloat,
       Utils.getPropClass(this.suiLoading, 'loading'),
+      Utils.getPropClass(this.suiLink, 'link'),
       'icon'
     ].joinWithWhitespaceCleanup();
   }
