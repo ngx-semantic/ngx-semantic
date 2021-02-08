e195b6467bc0dc6051c67b3cbacdaecafa631510
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-02-08 15:49:15 +0100

    #106 | add rotation, circular, bordered and inverted s icon support

--- lib/src/lib/elements/icon/icon.directive.ts
+++ lib/src/lib/elements/icon/icon.directive.ts
@@ -4,6 +4,7 @@ import {InputBoolean} from '../../core/util';
 
 export type SuiIconFloatDirection = 'right floated' | null;
 export type SuiIconFlipDirection = 'horizontal' | 'vertical' | null;
+export type SuiIconRotationDirection = 'clockwise' | 'counterclockwise' | null;
 
 @Directive({
   selector: '[sui-icon]',
@@ -13,11 +14,15 @@ export class SuiIconDirective {
   @Input() public suiFloat: SuiIconFloatDirection = null;
   @Input() public suiSize: SuiSize = null;
   @Input() public suiFlip: SuiIconFlipDirection = null;
+  @Input() public suiRotation: SuiIconRotationDirection = null;
   @Input() public suiIconType: string = null;
   @Input() @InputBoolean() public disabled = false;
   @Input() @InputBoolean() public suiLoading = false;
   @Input() @InputBoolean() public suiFitted = false;
   @Input() @InputBoolean() public suiLink = false;
+  @Input() @InputBoolean() public suiCircular = false;
+  @Input() @InputBoolean() public suiBordered = false;
+  @Input() @InputBoolean() public suiInverted = false;
 
   @HostBinding('class')
   get classes(): string {
@@ -26,6 +31,10 @@ export class SuiIconDirective {
       Utils.getPropClass(this.disabled, 'disabled'),
       this.suiSize,
       this.suiFlip ? `${this.suiFlip}ly flipped` : '',
+      this.suiRotation ? `${this.suiRotation} rotated` : '',
+      Utils.getPropClass(this.suiCircular, 'circular'),
+      Utils.getPropClass(this.suiBordered, 'bordered'),
+      Utils.getPropClass(this.suiInverted, 'inverted'),
       this.suiIconType,
       this.suiFloat,
       Utils.getPropClass(this.suiLoading, 'loading'),
