67fd0b2e911f0f8283758b76232ebdb345c1812d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-03-01 16:39:40 +0100

    ft: add card content

--- lib/src/lib/views/card/card.module.ts
+++ lib/src/lib/views/card/card.module.ts
@@ -2,15 +2,26 @@
  * Created by bolor on 8/17/2020
  */
 
-import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {SuiCardContentDirective} from './card-content.directive';
 import {SuiCardDirective} from './card.directive';
 import {SuiCardsDirective} from './cards.directive';
 
 @NgModule({
-  declarations: [SuiCardDirective, SuiCardsDirective],
-  imports: [CommonModule],
-  exports: [SuiCardDirective, SuiCardsDirective]
+  declarations: [
+    SuiCardDirective,
+    SuiCardsDirective,
+    SuiCardContentDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SuiCardDirective,
+    SuiCardsDirective,
+    SuiCardContentDirective
+  ]
 })
 export class SuiCardModule {
 }
--- lib/src/lib/views/card/public-api.ts
+++ lib/src/lib/views/card/public-api.ts
@@ -2,6 +2,7 @@
  * Created by bolor on 8/17/2020
  */
 
-export * from './card.directive';
+export * from './card-content.directive';
 export * from './cards.directive';
+export * from './card.directive';
 export * from './card.module';
