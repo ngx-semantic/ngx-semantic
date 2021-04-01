b81f1026e67235fefa143928c6477108ae17bbcd
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-04-01 13:32:53 +0100

    ft: do not export the dimmer

--- lib/src/lib/modules/dimmer/dimmer-content.directive.ts
+++ lib/src/lib/modules/dimmer/dimmer-content.directive.ts
@@ -2,11 +2,11 @@
  * Created by bolorundurowb on 1/6/2021
  */
 
-import {Directive} from '@angular/core';
-
-@Directive({
-  selector: '[suiDimmerContent]',
-  exportAs: 'suiDimmerContent'
-})
-export class SuiDimmerContentDirective {
-}
+// import {Directive} from '@angular/core';
+//
+// @Directive({
+//   selector: '[suiDimmerContent]',
+//   exportAs: 'suiDimmerContent'
+// })
+// export class SuiDimmerContentDirective {
+// }
--- lib/src/lib/modules/dimmer/index.ts
+++ lib/src/lib/modules/dimmer/index.ts
@@ -2,4 +2,4 @@
  * Created by bolorundurowb on 1/6/2021
  */
 
-export * from './public-api';
+// export * from './public-api';
