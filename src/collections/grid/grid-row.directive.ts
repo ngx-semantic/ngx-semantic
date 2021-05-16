b231858f8e416849a712072de7f939264a357700
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-05-16 08:13:55 +0100

    ft: fix row alignment

--- src/collections/grid/grid-row.directive.ts
+++ src/collections/grid/grid-row.directive.ts
@@ -7,7 +7,7 @@ import {SuiDeviceVisibility, SuiWidth} from 'ngx-semantic/core/enums';
 import {ClassUtils, InputBoolean} from 'ngx-semantic/core/util';
 import {BaseDirective} from 'ngx-semantic/core/base';
 
-export type SuiRowAlignment = 'left aligned' | 'center aligned' | 'right aligned' | null;
+export type SuiRowAlignment = 'left aligned' | 'middle aligned' | 'right aligned' | null;
 
 @Directive({
   exportAs: 'suiGridRow',
