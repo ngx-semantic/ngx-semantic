3ec6a2770ee9ff751e85fd982dc5b4a703492b06
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-19 14:30:37 +0100

    ft: update the form field directive

--- src/collections/form/form-field.directive.ts
+++ src/collections/form/form-field.directive.ts
@@ -2,29 +2,35 @@
  * Created by bolor on 6/5/2020
  */
 
-import {Directive, HostBinding, Input} from '@angular/core';
+import {Directive, ElementRef, Input} from '@angular/core';
 import {SuiWidth} from 'ngx-semantic/core/enums';
 import {ClassUtils, InputBoolean} from 'ngx-semantic/core/util';
+import {BaseDirective} from 'ngx-semantic/core/base';
 
 @Directive({
   exportAs: 'suiFormField',
   selector: '[suiFormField]'
 })
-export class SuiFormFieldDirective {
+export class SuiFormFieldDirective extends BaseDirective {
   @Input() public suiWidth: SuiWidth = null;
   @Input() @InputBoolean() public suiError = false;
   @Input() @InputBoolean() public suiInline = false;
   @Input() @InputBoolean() public disabled = false;
+  @Input() @InputBoolean() public suiRequired = false;
 
-  @HostBinding('class')
+  constructor(element: ElementRef) {
+    super(element);
+  }
+
   get classes(): string {
     return [
       ClassUtils.getPropClass(this.suiInline, 'inline'),
       this.suiWidth,
       this.suiWidth ? 'wide' : '',
       ClassUtils.getPropClass(this.disabled, 'disabled'),
+      ClassUtils.getPropClass(this.suiRequired, 'required'),
       'field',
-      ClassUtils.getPropClass(this.suiError, 'error'),
+      ClassUtils.getPropClass(this.suiError, 'error')
     ].join(' ');
   }
 }
