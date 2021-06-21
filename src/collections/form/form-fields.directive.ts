6837b7bfcafddf0fec0b0752fc41cf5dc0442bcd
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-21 18:09:23 +0100

    ft: update the form fields directive with teh equal width support

--- src/collections/form/form-fields.directive.ts
+++ src/collections/form/form-fields.directive.ts
@@ -2,25 +2,31 @@
  * Created by bolor on 6/5/2020
  */
 
-import {Directive, HostBinding, Input} from '@angular/core';
+import {Directive, ElementRef, Input} from '@angular/core';
 import {SuiWidth} from 'ngx-semantic/core/enums';
 import {ClassUtils, InputBoolean} from 'ngx-semantic/core/util';
+import {BaseDirective} from 'ngx-semantic/core/base';
 
 @Directive({
   exportAs: 'suiFormFields',
   selector: '[suiFormFields]'
 })
-export class SuiFormFieldsDirective {
+export class SuiFormFieldsDirective extends BaseDirective {
   @Input() public suiWidth: SuiWidth = null;
   @Input() @InputBoolean() public suiInline = false;
   @Input() @InputBoolean() public suiGrouped = false;
+  @Input() @InputBoolean() public suiEqualWidth = false;
 
-  @HostBinding('class')
+  constructor(element: ElementRef) {
+    super(element);
+  }
+
   get classes(): string {
     return [
       this.suiWidth,
       ClassUtils.getPropClass(this.suiInline, 'inline'),
       ClassUtils.getPropClass(this.suiGrouped, 'grouped'),
+      ClassUtils.getPropClass(this.suiEqualWidth, 'equal width'),
       'fields'
     ].join(' ');
   }
