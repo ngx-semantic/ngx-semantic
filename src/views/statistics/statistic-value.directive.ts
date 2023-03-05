1cc3babc3dbf42a30ced01e46c61a41b6ea80d7b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-03-05 21:18:19 +0100

    ft: add in text value types for statistics

--- src/views/statistics/statistic-value.directive.ts
+++ src/views/statistics/statistic-value.directive.ts
@@ -2,15 +2,21 @@
  * Created by bolor on 7/16/2020
  */
 
-import {Directive, HostBinding} from '@angular/core';
+import {Directive, HostBinding, Input} from '@angular/core';
+import {ClassUtils, InputBoolean} from 'ngx-semantic/core/util';
 
 @Directive({
   exportAs: 'suiStatValue',
   selector: '[suiStatValue]'
 })
 export class SuiStatisticValueDirective {
+  @Input() @InputBoolean() public suiText = false;
+
   @HostBinding('class')
-  get classes(): string {
-    return 'value';
+  get classes(): string[] {
+    return [
+      ClassUtils.getPropClass(this.suiText, 'text'),
+      'value'
+    ];
   }
 }
