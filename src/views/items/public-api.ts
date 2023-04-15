51449d58855a7e29322e7516bc79ff44e1b8b1e2
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-04-16 00:47:40 +0100

    ft: fix item directive exports

--- src/views/items/public-api.ts
+++ src/views/items/public-api.ts
@@ -2,6 +2,11 @@
  * Created by bolor on 8/2/2020
  */
 
+export * from './item-content.directive';
+export * from './item-header.directive';
+export * from './item-metadata.directive';
+export * from './item-extra-content.directive';
+export * from './item-description.directive';
 export * from './item.directive';
 export * from './items.directive';
 export * from './items.module';
