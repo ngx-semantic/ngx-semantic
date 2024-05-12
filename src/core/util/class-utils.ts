09cd4ac1b95ab29282b16f921684a155205ad732
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-12 21:39:09 +0100

    ft: add in helper utils for classes

--- src/core/util/class-utils.ts
+++ src/core/util/class-utils.ts
@@ -6,4 +6,12 @@ export class ClassUtils {
 
     return className;
   }
+
+  public static combineToClass(input: string[]): string {
+    return input.filter(x => !!x).join(' ').trim();
+  }
+
+  public static removeExcessWhitespace(input: string): string {
+    return  input.replace(/\s\s+/g, ' ').trim();
+  }
 }
